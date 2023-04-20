723b12bd8f6254c5c59c7338556335c6e6671484
Joni <jonigonzalez2492@gmail.com>
2023-04-20 02:28:51 -0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+gsap.registerPlugin(Flip);
+
+const links = document.querySelectorAll(".nav-item a");
+const activeNav = document.querySelector(".active-nav");
+
+links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        gsap.to(links, {color: "#252525"});
+        if(document.activeElement === link) {
+            gsap.to(link, {color: "#385ae0"});
+        }
+
+        //Move the line under links
+        const state = Flip.getState(activeNav);
+        link.appendChild(activeNav);
+        Flip.from (state, {
+            duration: 1.25,
+            absolute: true,
+            ease: 'elastic.out(1,0.5)',
+        });
+    });
+});
+
+
+// Cards
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card, index) => {
+    card.addEventListener('click', () => {
+        // Get state
+        const state = Flip.getState(cards);
+
+        // Add active class with click and add inactive to others
+        const isCardActive = card.classList.contains("active");
+        cards.forEach((otherCard, otherIdx) => {
+            otherCard.classList.remove('active');
+            otherCard.classList.remove('is-inactive');
+            if(!isCardActive && index !== otherIdx) {
+                otherCard.classList.add("is-inactive");
+            }
+        });
+        if(!isCardActive) card.classList.add('active');
+
+        Flip.from(state, {
+            duration: 1,
+            ease: "expo.out",
+            absolute: true,
+        });
+    });
+});
